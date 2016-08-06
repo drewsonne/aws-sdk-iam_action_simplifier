e4e968e7451962fd009caa62b65a3727a1237644
Drew J. Sonne <drew.sonne@nordcloud.com>
2016-08-06 07:30:57 +0100

    Initial Commit

---
+++ lib/aws/sdk/iam_action_simplifier.rb
@@ -0,0 +1,9 @@
+require "aws/sdk/iam_action_simplifier/version"
+
+module Aws
+  module Sdk
+    module IamActionSimplifier
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/aws/sdk/iam_action_simplifier/version.rb
@@ -0,0 +1,7 @@
+module Aws
+  module Sdk
+    module IamActionSimplifier
+      VERSION = "0.1.0"
+    end
+  end
+end
